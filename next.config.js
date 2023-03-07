aa9b06c28793471fdabc86ca44d7c7c1d7046bce
Ervan Ade Wijaya <ervanade26@gmail.com>
2023-03-07 14:22:39 +0700

    add config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,29 @@
 /** @type {import('next').NextConfig} */
+const fs = require('fs');
+const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   
 }
+module.exports = {
+  exportPathMap: async function (defaultPathMap) {
+    const dynamicRoutes = await fs.readdirSync('./pages/articles');
+    const dynamicRoutesMap = dynamicRoutes.reduce((acc, route) => {
+      if (route.endsWith('.js')) {
+        const name = route.replace(/\.js$/, '');
+        acc[`/article/${name}`] = { page: '/articles/[id]', query: { id: name } };
+      }
+      return acc;
+    }, {});
+
+    return {
+      '/': { page: '/' },
+      '/article': { page: '/article' },
+      '/testimoni': { page: '/testimoni' },
+      ...dynamicRoutesMap,
+    };
+  },
+};
 
 module.exports = nextConfig

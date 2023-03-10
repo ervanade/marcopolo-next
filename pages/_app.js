c2ea93f11b6c87b1b226478e618b7f43f3e01ba8
Ervan Ade Wijaya <eawijaya@cpxi-asia.com>
2023-03-10 13:48:05 +0700

    add gtm

--- pages/_app.js
+++ pages/_app.js
@@ -11,14 +11,20 @@ import './components/hero/Hero.css'
 import './article/ArticlePage.css'
 import './components/modal/Modal.css'
 import 'nprogress/nprogress.css';
+import { useEffect } from 'react';
 
 import Navbar from './components/navbar/Navbar';
 
 import Footer from './components/footer/Footer';
 import './components/footer/Footer.css'
 import Layout from './components/Layout';
+import TagManager from 'react-gtm-module';
+
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-MPJW2JV" });
+  }, []);
   return (
   <Layout>
   <Component {...pageProps} />

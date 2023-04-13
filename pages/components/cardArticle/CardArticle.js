092c67522e76773da63498a2ad45afd14b2a5a0d
Ervan Ade Wijaya <ervanade26@gmail.com>
2023-04-13 15:26:16 +0700

    split to component 1

--- pages/components/cardArticle/CardArticle.js
+++ pages/components/cardArticle/CardArticle.js
@@ -9,14 +9,14 @@ const CardArticle = ({Title, Image, Excerpt, Links}) => {
   return (
     <div className="card__article">
     <div className="card__article__image">
-    <Link draggable="true" onDragStart={handleDragStart} href={Links}>
+    <Link draggable="true" onDragStart={handleDragStart} href={Links || "#"}>
     <img src={Image} alt={`gambar-${Title}`} />
    </Link>
     </div>
     <div className="card__description">
-     <Link draggable="true" onDragStart={handleDragStart} href={Links}><h1>{Title?.split(" ").slice(0, 7).join(" ")}..</h1></Link>
+     <Link draggable="true" onDragStart={handleDragStart} href={Links || "#"}><h1>{Title?.split(" ").slice(0, 7).join(" ")}..</h1></Link>
      <p>{Excerpt?.split(" ").slice(0, 7).join(" ")}...</p>
-     <Link href={Links}><button className='button__explore' name="button__explore">Explore<MdTravelExplore className="button__icon" size={20}/></button></Link>
+     <Link href={Links || "#"}><button className='button__explore' name="button__explore">Explore<MdTravelExplore className="button__icon" size={20}/></button></Link>
      </div>
    </div>
   )

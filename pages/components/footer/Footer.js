2d189384890be074a4b4ecfbb31fb69b71870699
Ervan Ade Wijaya <89782667+ervanade@users.noreply.github.com>
2025-06-04 20:09:32 +0700

    fix

--- pages/components/footer/Footer.js
+++ pages/components/footer/Footer.js
@@ -4,17 +4,19 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='section__footer'>
-        <div className="container">
-            <div className="wrapper__footer">
-                <div className="logo"><img src={`${process.env.NEXT_PUBLIC_APP_PUBLIC_URL}/assets/ghw_icon.png`} alt="gambar_bahaya_merokok" /></div>
-                <div className='perhatian_footer'>
-                  <p className='!font-semibold'>PERINGATAN:</p>
-                  <p>KARENA MEROKOK, SAYA TERKENA KANKER TENGGOROKAN. LAYANAN BERHENTI MEROKOK</p>
-                  <p>(0800-177-6565)</p>
-                  </div>
-                <div className="lingkar_umur"> 21+</div>
-            </div>
+      <div className="container">
+        <div className="wrapper__footer">
+          <div className="logo"><img src={`${process.env.NEXT_PUBLIC_APP_PUBLIC_URL}/assets/ghw_icon.png`} alt="gambar_bahaya_merokok" /></div>
+          <div className='perhatian_footer'>
+            <p className='!font-semibold'>PERINGATAN:</p>
+            <p>KARENA MEROKOK, SAYA TERKENA KANKER TENGGOROKAN. </p>
+            <p> LAYANAN BERHENTI MEROKOK. (0800-177-6565)</p>
+            <p>DILARANG MENJUAL DAN MEMBERI KEPADA ORANG</p>
+            <p>DIBAWAH 21 TAHUN DAN PEREMPUAN HAMIL.</p>
+          </div>
+          <div className="lingkar_umur"> <p>21+</p></div>
         </div>
+      </div>
     </div>
   )
 }
